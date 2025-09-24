d8c0f1b507405c314f040336755d923c995b4a92
rlkandela <rlkandela@gmail.com>
2025-09-25 01:18:19 +0200

    added qemu and formatted

--- flake.nix
+++ flake.nix
@@ -9,102 +9,102 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
-  let
+    let
     configuration = { pkgs, config, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
+# List packages installed in system profile. To search by name, run:
+# $ nix-env -qaP | grep wget
       environment.systemPackages =
         [
-    pkgs.mkalias
-    pkgs.neovim
-    pkgs.nerd-fonts.jetbrains-mono
-    pkgs.pyenv
-    pkgs.nodejs_24
-    pkgs.awscli2
-    pkgs.ripgrep
-    pkgs.minikube
-    pkgs.acme-sh
-    pkgs.qemu
+        pkgs.mkalias
+          pkgs.neovim
+          pkgs.nerd-fonts.jetbrains-mono
+          pkgs.pyenv
+          pkgs.nodejs_24
+          pkgs.awscli2
+          pkgs.ripgrep
+          pkgs.minikube
+          pkgs.acme-sh
         ];
 
-	homebrew = {
-	  enable = true;
-	  casks = [
-            "arc"
-	    "the-unarchiver"
-	    "iina"
-	    "hammerspoon"
-	    "iterm2"
-	    "obsidian"
-	    "hiddenbar"
-	    "openvpn-connect"
-	    "github"
-      "warp"
-	  ];
-	  brews = [
-	    "gpg2"
-	    "gnupg"
-	    "pinentry-mac"
-	    "nvm"
-	    "wireshark"
-	    "readline"
-	    "xz"
-      "jj"
-      "kind"
-      "kubernetes-cli"
-	  ];
-	  masApps = {
-            # "AppName" = <app id>;
-	  };
-	  onActivation.cleanup = "zap";
-	  onActivation.autoUpdate = true;
-	  onActivation.upgrade = true;
-	};
-
-	# Allowed unfree licenses
-        nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-          # "obsidian"
+      homebrew = {
+        enable = true;
+        casks = [
+          "arc"
+          "the-unarchiver"
+          "iina"
+          "hammerspoon"
+          "iterm2"
+          "obsidian"
+          "hiddenbar"
+          "openvpn-connect"
+          "github"
+          "warp"
         ];
+        brews = [
+          "gpg2"
+          "gnupg"
+          "pinentry-mac"
+          "nvm"
+          "wireshark"
+          "readline"
+          "xz"
+          "jj"
+          "kind"
+          "kubernetes-cli"
+          "qemu"
+        ];
+        masApps = {
+# "AppName" = <app id>;
+        };
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
 
-      # Make apps installed by nix appear in spotlight search
+# Allowed unfree licenses
+      nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+# "obsidian"
+      ];
+
+# Make apps installed by nix appear in spotlight search
       system.activationScripts.applications.text = let
         env = pkgs.buildEnv {
-	  name = "system-applications";
-	  paths = config.environment.systemPackages;
-	  pathsToLink = "/Applications";
-	};
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
       in
         pkgs.lib.mkForce ''
-	  # Set up applications
-	  echo "setting up /Applications..." >&2
-	  rm -rf /Applications/Nix\ Apps
-	  mkdir -p /Applications/Nix\ Apps
-	  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-	  while read -r src; do
-	    app_name=$(basename "$src")
-	    echo "copying $src" >&2
-	    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-	  done
-	'';
+# Set up applications
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read -r src; do
+          app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+            '';
 
 
-      # Necessary for using flakes on this system.
+# Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+# Enable alternative shell support in nix-darwin.
+# programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
+# Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
+# Used for backwards compatibility, please read the changelog before changing.
+# $ darwin-rebuild changelog
       system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
+# The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
 
-      # System settings
+# System settings
       security.pam.services.sudo_local.touchIdAuth = true;
       system = {
         primaryUser = "raulcandelaarias";
@@ -112,76 +112,76 @@
           enableKeyMapping = true;
           remapCapsLockToEscape = true;
         };
-	defaults = {
-	  controlcenter = {
-	    BatteryShowPercentage = true;
-	  };
-	  trackpad = {
-	    Clicking = true;
-	    Dragging = true;
-	  };
+        defaults = {
+          controlcenter = {
+            BatteryShowPercentage = true;
+          };
+          trackpad = {
+            Clicking = true;
+            Dragging = true;
+          };
           NSGlobalDomain = {
             "com.apple.mouse.tapBehavior" = 1;
-	    "com.apple.keyboard.fnState" = false;
-	    ApplePressAndHoldEnabled = false;
+            "com.apple.keyboard.fnState" = false;
+            ApplePressAndHoldEnabled = false;
           };
-	  dock = {
-	    minimize-to-application = false;
-	    mru-spaces = false;
-	    show-recents = false;
-	    persistent-apps = [
+          dock = {
+            minimize-to-application = false;
+            mru-spaces = false;
+            show-recents = false;
+            persistent-apps = [
               "/System/Applications/System Settings.app"
-              "/System/Cryptexes/App/System/Applications/Safari.app"
-	      # "/Applications/Microsoft Outlook.app"
-	      "/Applications/Arc.app"
-	      "/Applications/iTerm.app"
-	      "/Applications/Docker.app"
-	      "/Applications/Visual Studio Code.app"
-	      "/Applications/Spotify.app"
-	      "/Applications/Obsidian.app"
-        "/Users/raulcandelaarias/Applications/Excalidraw.app"
-        "/Users/raulcandelaarias/Applications/To Do.app"
-        "/Users/raulcandelaarias/Applications/Clash Ninja.app"
-	    ];
-	  };
-	  finder = {
+                "/System/Cryptexes/App/System/Applications/Safari.app"
+# "/Applications/Microsoft Outlook.app"
+                "/Applications/Arc.app"
+                "/Applications/iTerm.app"
+                "/Applications/Docker.app"
+                "/Applications/Visual Studio Code.app"
+                "/Applications/Spotify.app"
+                "/Applications/Obsidian.app"
+                "/Users/raulcandelaarias/Applications/Excalidraw.app"
+                "/Users/raulcandelaarias/Applications/To Do.app"
+                "/Users/raulcandelaarias/Applications/Clash Ninja.app"
+            ];
+          };
+          finder = {
             _FXShowPosixPathInTitle = false;
-	    _FXSortFoldersFirst = true;
-	    AppleShowAllExtensions = true;
-	    AppleShowAllFiles = true;
-	    FXEnableExtensionChangeWarning = true;
-	    FXPreferredViewStyle = "clmv";
-	    NewWindowTarget = "Home";
-	    ShowPathbar = true;
-	    ShowExternalHardDrivesOnDesktop = true;
-	    ShowMountedServersOnDesktop = true;
-	    ShowRemovableMediaOnDesktop = true;
-	  };
-	};
+            _FXSortFoldersFirst = true;
+            AppleShowAllExtensions = true;
+            AppleShowAllFiles = true;
+            FXEnableExtensionChangeWarning = true;
+            FXPreferredViewStyle = "clmv";
+            NewWindowTarget = "Home";
+            ShowPathbar = true;
+            ShowExternalHardDrivesOnDesktop = true;
+            ShowMountedServersOnDesktop = true;
+            ShowRemovableMediaOnDesktop = true;
+          };
+        };
       };
     };
 
-    # Nix Homebrew
-    nix-brew = { pkgs, ... }: nix-homebrew.darwinModules.nix-homebrew
-      {
-        lib = pkgs.lib;
-        nix-homebrew = {
-          enable = true;
-          # Apple Silicon Only
-          enableRosetta = true;
-          # User owning the Homebrew prefix
-          user = "raulcandelaarias";
-          mutableTasp = false;
-        };
-      };
+# Nix Homebrew
+  nix-brew = { pkgs, ... }: nix-homebrew.darwinModules.nix-homebrew
+  {
+    lib = pkgs.lib;
+    nix-homebrew = {
+      enable = true;
+# Apple Silicon Only
+      enableRosetta = true;
+# User owning the Homebrew prefix
+      user = "raulcandelaarias";
+      mutableTasp = false;
+    };
+  };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#m4pro
+# Build darwin flake using:
+# $ darwin-rebuild build --flake .#m4pro
     darwinConfigurations."m4pro" = nix-darwin.lib.darwinSystem {
       modules = [
         configuration
-	      nix-brew
+          nix-brew
       ];
     };
   };
