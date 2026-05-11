0e0574828e16b519cf53d48fe3d448efc7151443
rlkandela <rlkandela@gmail.com>
2026-05-11 11:18:55 +0200

    fixed merging error

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,8 +1,13 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     inputs.flake-parts.flakeModules.modules
   ];
+
+  options.flake.darwinConfigurations = lib.mkOption {
+    type = lib.types.lazyAttrsOf lib.types.unspecified;
+    default = { };
+  };
 
   config = {
     systems = [
