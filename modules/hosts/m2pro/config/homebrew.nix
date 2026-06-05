4045c55a0d94fee70ef8c9b189cfef0aba71f72c
rlkandela <rlkandela@gmail.com>
2026-06-05 11:24:32 +0200

    preventing future breaking change for homebrew ZAP cleanup

--- modules/hosts/m2pro/config/homebrew.nix
+++ modules/hosts/m2pro/config/homebrew.nix
@@ -1,30 +1,31 @@
-{ ... }:
-{
-  flake.modules.darwin.m2proHomebrew =
-    { ... }:
-    {
-      homebrew = {
-        enable = true;
-        casks = [
-          "diffmerge"
-          "warp"
-          "qt-creator"
-        ];
-        brews = [
-          "mas"
-          "gettext"
-          "jj"
-          "qt6"
-          "cmake"
-        ];
-        masApps = {
-          "Yoink" = 457622435;
-          "PowerPoint" = 462062816;
-	  "CotEditor" = 1024640650;
-        };
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
+_: {
+  flake.modules.darwin.m2proHomebrew = _: {
+    homebrew = {
+      enable = true;
+      casks = [
+        "diffmerge"
+        "warp"
+        "qt-creator"
+      ];
+      brews = [
+        "mas"
+        "gettext"
+        "jj"
+        "qt6"
+        "cmake"
+      ];
+      masApps = {
+        "Yoink" = 457622435;
+        "PowerPoint" = 462062816;
+        "CotEditor" = 1024640650;
+      };
+      onActivation = {
+        # Using ZAP requires to use --force
+        cleanup = "zap";
+        extraFlags = ["--force"];
+        autoUpdate = true;
+        upgrade = true;
       };
     };
+  };
 }
--- modules/hosts/m4pro/config/homebrew.nix
+++ modules/hosts/m4pro/config/homebrew.nix
@@ -1,47 +1,44 @@
-{
-  ...
-}:
-{
-  flake.modules.darwin.m4proHomebrew =
-    { ... }:
-    {
-
-      homebrew = {
-        enable = true;
-        casks = [
-          "arc"
-          "vlc"
-          "the-unarchiver"
-          "iina"
-          "hammerspoon"
-          "iterm2"
-          "obsidian"
-          "hiddenbar"
-          "openvpn-connect"
-          "github"
-          "warp"
-        ];
-        brews = [
-          "gpg2"
-          "gnupg"
-          "pinentry-mac"
-          "nvm"
-          "wireshark"
-          "readline"
-          "xz"
-          "jj"
-          "kind"
-          "qemu"
-          "watch"
-          "luarocks"
-        ];
-        masApps = {
-          # "AppName" = <app id>;
-        };
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
+_: {
+  flake.modules.darwin.m4proHomebrew = _: {
+    homebrew = {
+      enable = true;
+      casks = [
+        "arc"
+        "vlc"
+        "the-unarchiver"
+        "iina"
+        "hammerspoon"
+        "iterm2"
+        "obsidian"
+        "hiddenbar"
+        "openvpn-connect"
+        "github"
+        "warp"
+      ];
+      brews = [
+        "gpg2"
+        "gnupg"
+        "pinentry-mac"
+        "nvm"
+        "wireshark"
+        "readline"
+        "xz"
+        "jj"
+        "kind"
+        "qemu"
+        "watch"
+        "luarocks"
+      ];
+      masApps = {
+        # "AppName" = <app id>;
       };
-
+      onActivation = {
+        # Using ZAP requires to use --force
+        cleanup = "zap";
+        extraFlags = ["--force"];
+        autoUpdate = true;
+        upgrade = true;
+      };
     };
+  };
 }
